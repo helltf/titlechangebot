b17e06faf6cfd77caa68cab9a62a5915dcd63d1e
helltf <74050664+helltf@users.noreply.github.com>
2021-05-11 13:27:56 +0200

    added ReadME reworked script

--- botstartup.js
+++ botstartup.js
@@ -3,14 +3,26 @@ const botconfig= require('./config.json');
 const request = require('request');
 const tmi = require('tmi.js');
 const fs = require('fs');
-var channelListJSON = fs.readFileSync("./channels.json");
-var channelList = JSON.parse(channelListJSON);
+const fspromises =require("fs").promises
+let channelListJSON = fs.readFileSync("./users.json");
+let channelList = JSON.parse(channelListJSON);
+let AT = ""
+const broadcaster = process.env.BROADCASTER
+
 const DEFAULT_COOLDOWN={
     DEFAULT_TITLE_COOLDOWN:60000,
     DEFAULT_GAME_COOLDOWN:5000,
     DEFAULT_LIVE_COOLDOWN:60000*6
   }
-let AT = ""
+
+  let channelstatus = {
+    title:undefined,
+    live:undefined,
+    game_id:undefined,
+    game_cooldown:false,
+    live_cooldown:false,
+    title_cooldown:false
+}
 
 const client= new tmi.Client(botconfig);
 
@@ -25,33 +37,40 @@ client.on('chat',  (channel,user,message,self)=> {
         if(messageparts.length!=2) return
         let event = messageparts[1]
         if(event.search(/^live$|^offline$|^title$|^game$/)===-1) return client.say(channel,"Wrong event!")
-         if(!channelList.kian[event].includes(user.username)){
+         if(!channelList.broadcaster[event].includes(user.username)){
             return client.say(channel,"You are not registered")
          }            
-        channelList.kian[event].splice(channelList.kian[event].indexOf(user.username),1)
+        channelList.broadcaster[event].splice(channelList.broadcaster[event].indexOf(user.username),1)
+        client.say(channel,`Succesfully removed you from ${event} event`)
+        console.log(`removed ${user?.username} from ${event}`)
          toJSON()
     }
+
     if(message.startsWith("!notifyme")){
         if(messageparts.length!=2) return
         let event = messageparts[1]
         if(event.search(/^live$|^offline$|^title$|^game$/)===-1) return client.say(channel,"Wrong event!")
-         if(channelList.kian[event].includes(user.username)){
+         if(channelList.broadcaster[event].includes(user.username)){
              return client.say(channel,"You are already registered")
          }
-         channelList.kian[event].push(user.username)
+         channelList.broadcaster[event].push(user.username)
+         client.say(channel,`Succesfully added you to ${event} event`)
+         console.log(`added ${user?.username} to ${event}`)
          toJSON()
     }
 });
-const toJSON = ()=>{
-    cooldown=true;
-    fs.writeFile("./channels.json", JSON.stringify(channelList), function writeJSON(err) {
+
+const toJSON = async()=>{
+    await fspromises.writeFile("./users.json", JSON.stringify(channelList), function writeJSON(err) {
         if (err) return console.log(err);
     });
+     channelListJSON = fs.readFileSync("./users.json");
+     channelList = JSON.parse(channelListJSON);
 }
 
 async function initializeAT(){
     const options={
-        url: process.env.TOKEN_URL,
+        url: "https://id.twitch.tv/oauth2/token",
         json:true,
         body:{
             client_id:process.env.CLIENT_ID,
@@ -59,34 +78,43 @@ async function initializeAT(){
             grant_type:'client_credentials'
         }
     };
-    let response = await postrequest(options)
-    AT=response?.access_token
+    try{
+      let response = await postrequest(options)
+      AT=response?.access_token
+    }catch(err){
+      console.log(err)
+    }
+
 };
 
 async function doChannelAPIUpdate(){
-    const options={
-        url:process.env.GET_LIVE + "kian",
+    const live_options={
+        url:"https://api.twitch.tv/helix/search/channels?query=" + broadcaster,
         method:'GET',
         headers:{
             'Client-ID':process.env.CLIENT_ID,
             'Authorization': 'Bearer ' + AT
         }
     };
-        let response= await asyncrequest(options)
-        response.data.forEach((streamer)=>{
-            if(streamer.broadcaster_login==="kian"){
-                updateChannelProperty({
-                            title:streamer.title,
-                            game_id:streamer.game_id,
-                            live:streamer.is_live 
-                })
-            }
-        })
+    try {
+      let response= await asyncrequest(live_options)
+      response.data.forEach((streamer)=>{
+          if(streamer.broadcaster_login===broadcaster){
+              updateChannelProperty({
+                          title:streamer.title,
+                          game_id:streamer.game_id,
+                          live:streamer.is_live 
+              })
+          }
+      })
+    }catch(e){
+console.log(e)
     }
+  }
 async function updateChannelProperty(channelinfo){
     for(let[key,value] of Object.entries(channelinfo)){
         if(value!=undefined&&channelstatus[key]!=undefined&&value!=channelstatus[key]){
-            notify(key,value,"kian")
+            notify(key,value)
             channelstatus[key]=value
         }
     }
@@ -106,7 +134,6 @@ const getGameByID=async(id)=>{
          reject(err)
          console.log(err)
       }
-      console.log(body)
       let parsed = JSON.parse(body)
       if(parsed.data.length===0) resolve(undefined) 
       resolve(parsed?.data[0]?.name)
@@ -114,25 +141,41 @@ const getGameByID=async(id)=>{
   })
   }
 
-async function notify(key, value,streamer){
+async function notify(key, value){
     if(key==="game_id")  key="game";
     if(key==="game") value = await getGameByID(value)
+    if(channelstatus[`${key}_cooldown`]===true) return
     if(value===undefined) return;
+    if (key==="live"&&!value) key ="offline"
+
+    let emote = process.env[`${key.toUpperCase()}_EMOTE`]
     switch(key){
         case "live": 
-          if (value===true) {
-            beginmessage = `PagMan 👉  ${streamer} went live DinkDonk `;
+            beginmessage = `${emote} 👉 ${broadcaster} has gone live ${emote} 👉 `
             break
-          }
-            beginmessage = `FeelsBadMan 👉  ${streamer} went offline DinkDonk `; break;
-        default: beginmessage = `PagMan 👉  ${streamer} has changed the ${key} to ${value} DinkDonk `; break;
+        case "offline":
+            beginmessage = `${emote} 👉 ${broadcaster} has gone offline ${emote} 👉 `
+            break
+
+        case "title":
+          beginmessage = `${emote} NEW TITLE! ${emote} 👉 `
+          break
+
+        case "game": 
+          beginmessage = `${emote} NEW GAME! ${emote} 👉 ${value} 👉`
+          break
+        }
+        channelstatus[`${key}_cooldown`] = true
+
+    let messages = splitUserMessage(channelList.broadcaster[key],beginmessage)
+    for await(message of messages){
+      if(message===beginmessage) continue
+      client.action(broadcaster,`${message}`)
+      await timer(2000)
     }
-    if(channelstatus[`${key}_cooldown`]===true) return
-    if (key==="live"&&!value) key ="offline"
-    let messages = splitUserMessage(channelList.kian[key],beginmessage)
-    messages.forEach((message)=>{client.say("kian",`${message}`)})
+
     if(key==="offline") key = "live"
-    channelstatus[`${key}_cooldown`] = true
+
     setTimeout(()=>{
         channelstatus[`${key}_cooldown`]=false
     },DEFAULT_COOLDOWN[`DEFAULT_${key.toUpperCase()}_COOLDOWN`])
@@ -142,7 +185,7 @@ const splitUserMessage=(users,beginmessage)=>{
     let messageArray=[]
     let currentmessage=beginmessage
     for(user of users){
-      if((currentmessage+" "+user).length<500){
+      if((currentmessage+" "+user).length<480){
         currentmessage+=" "+user
       }else{
         messageArray.push(currentmessage)
@@ -153,48 +196,28 @@ const splitUserMessage=(users,beginmessage)=>{
     return messageArray
   }
 
-async function connect(){
-    await client.connect()
-    await initializeAT()
-    initChannelstatu()
-    setInterval(doChannelAPIUpdate,2*1000);
-    setInterval(initializeAT,3300000)
-}
-let channelstatus = {
-    title:undefined,
-    live:undefined,
-    game_id:undefined,
-    game_cooldown:false,
-    live_cooldown:false,
-    title_cooldown:false
-}
-const initChannelstatu=async()=>{
-    const options={
-        url:process.env.GET_LIVE + "kian",
+const initChannelstatus=async()=>{
+    const live_options={
+        url:"https://api.twitch.tv/helix/search/channels?query=" + broadcaster,
         method:'GET',
         headers:{
             'Client-ID':process.env.CLIENT_ID,
             'Authorization': 'Bearer ' + AT
         }
     };
-        let response= await asyncrequest(options)
+        let response= await asyncrequest(live_options)
         response.data.forEach((streamer)=>{
-            if(streamer.broadcaster_login==="kian"){
+            if(streamer.broadcaster_login===broadcaster){
                 channelstatus.game_id=streamer.game_id
                 channelstatus.title=streamer.title
-                channelstatus.live=streamer.live
+                channelstatus.live=streamer.is_live
             }
         })
 }
 const postrequest= async(options)=>{
     return new Promise((resolve,reject)=>{
       request.post(options,(err,res,body)=>{
-        if(err){
-          reject(err);
-        }
-        else{
-          resolve(body)
-        }
+        err ? reject(err) : resolve(body)
       })
     })
   }
@@ -202,18 +225,26 @@ const postrequest= async(options)=>{
 const asyncrequest=async(options)=>{
     return new Promise((resolve,reject)=>{
       request.get(options,(err,res,body)=>{
-        if(err){
-          reject(err);
-        }
-        else{
+        if(err) reject(err);
           try{
             resolve(JSON.parse(body))
           }catch(err){
             console.log(err)
             resolve(undefined)
-          }
         }
       })
     })
   }
-  connect()
+
+const timer= ms => new Promise(res => setTimeout(res, ms))
+
+async function connect(){
+  await client.connect()
+  await client.join(broadcaster)
+  await initializeAT()
+  await initChannelstatus()
+  setInterval(doChannelAPIUpdate,5*1000);
+  setInterval(initializeAT,3300000)
+}
+
+connect()
